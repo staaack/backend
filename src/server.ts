96e39b4d86f2bca836cbcf67bacdbf754afc4150
Wang Dan <apaladin1993@outlook.com>
2020-01-30 23:12:54 +0800

    Fix command on docker

--- src/server.ts
+++ src/server.ts
@@ -5,10 +5,7 @@ if (result.error) {
 }
 
 import app from './app';
-import MongoConnection from './mongo-connection';
 import logger from './logger';
-
-const mongoConnection = new MongoConnection(process.env.MONGO_URL);
 
 if (process.env.MONGO_URL == undefined) {
   logger.log({
@@ -18,27 +15,15 @@ if (process.env.MONGO_URL == undefined) {
   process.exit(1);
   process.stdin.emit('SIGINT');
 } else {
-  mongoConnection.connect(() => {
-    app.listen(app.get('port'), (): void => {
-      logger.info(`*\t🌏 Express server started at http://localhost:${app.get('port')}\t\t*`);
-      if (process.env.NODE_ENV === 'development') {
-        logger.debug(`*\t⚙️  Swagger UI hosted at http://localhost:${app.get('port')}/dev/api-docs\t*`);
-      }
-    });
+  app.listen(app.get('port'), (): void => {
+    logger.info(`*\t🌏 Express server started at http://localhost:${app.get('port')}\t\t*`);
+    if (process.env.NODE_ENV === 'development') {
+      logger.debug(`*\t⚙️  Swagger UI hosted at http://localhost:${app.get('port')}/dev/api-docs\t*`);
+    }
   });
 }
 
 // Close the Mongoose connection, when receiving SIGINT
 process.on('SIGINT', () => {
   logger.info('\nGracefully shutting down');
-  mongoConnection.close(err => {
-    if (err) {
-      logger.log({
-        level: 'error',
-        message: 'Error shutting closing mongo connection',
-        error: err,
-      });
-    }
-    process.exit(0);
-  });
 });
