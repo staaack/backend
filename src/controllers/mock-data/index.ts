58df0f16f1daea2f020c27f898e5c0f328342717
Wang Dan <apaladin1993@outlook.com>
2020-02-07 02:18:32 +0800

    Enhancement ( remove user interaction on reading)

--- src/controllers/mock-data/index.ts
+++ src/controllers/mock-data/index.ts
@@ -1,6 +1,6 @@
 import { RequestHandler } from 'express';
 import handleErrorMiddleware from '../../middleware/handle-error-middleware';
-import * as read from './read';
+import * as reader from './read';
 import { readFile } from 'fs';
 
 var mock_data: any;
@@ -8,28 +8,14 @@ var mock_data: any;
 readFile('src/res/credentials.json', (err, content) => {
   if (err) return console.log('Error loading client secret file:', err);
 
-  read.default(JSON.parse(content.toString('utf8')), (res: any) => {
-    mock_data = res;
+  reader.default.read((res: any) => {
+    mock_data = res
   });
 });
 
 
 let get: RequestHandler = async (req, res) => {
-  res.send({
-  	"Engineering": mock_data[0]['values'],
-  	"Company information": {
-  		"Employee list": mock_data[1]['values'],
-  		"Clients list": mock_data[2]['values'],
-  		"Departments": mock_data[3]['values']
-  	},
-  	"User information": mock_data[4]['values'],
-  	"Timesheet information": mock_data[5]['values'],
-  	"Project information": mock_data[6]['values'],
-  	"Sales": {
-  		"Financial data": mock_data[7]['values'],
-  		"Financial Metrics": mock_data[8]['values']
-  	}
-  });
+  res.send(mock_data);
 };
 
 get = handleErrorMiddleware(get);
--- src/controllers/mock-data/read.ts
+++ src/controllers/mock-data/read.ts
@@ -1,72 +1,34 @@
-import { google } from 'googleapis';
-import * as readline from 'readline';
-import * as fs from 'fs';
+import * as credential from '../../res/interface-213701-f50d26f863b3.json';
 
-// If modifying these scopes, delete token.json.
-const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
-// The file token.json stores the user's access and refresh tokens, and is
-// created automatically when the authorization flow completes for the first
-// time.
-const TOKEN_PATH = 'token.json';
+const { GoogleSpreadsheet } = require('google-spreadsheet');
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- * @param {google.auth.OAuth2} oAuth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback for the authorized client.
- */
-function getNewToken(oAuth2Client: any, callback: any) {
-  const authUrl = oAuth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES,
-  });
-  console.log('Authorize this app by visiting this url:', authUrl);
-  const rl = readline.createInterface({
-    input: process.stdin,
-    output: process.stdout,
-  });
-  rl.question('Enter the code from that page here: ', (code) => {
-    rl.close();
-    oAuth2Client.getToken(code, (err: any, token: any) => {
-      if (err) return console.error('Error while trying to retrieve access token', err);
-      oAuth2Client.setCredentials(token);
-      readData(oAuth2Client, callback);
-    });
-  });
+
+async function read(callback: any){
+
+  const doc = new GoogleSpreadsheet('1C_GqEIdO_F8M-WuxsYoFLK7-I8XNL6hmtr8V33wmGLU');
+
+  await doc.useServiceAccountAuth(credential);
+  await doc.loadInfo();
+
+  var mockData = {
+    "Engineering": await doc.sheetsByIndex[0].getCellsInRange('A6:R'),
+    "Company information": {
+      "Employee list": await doc.sheetsByIndex[1].getCellsInRange('A10:B14'),
+      "Clients list": await doc.sheetsByIndex[1].getCellsInRange('D10:F14'),
+      "Departments": await doc.sheetsByIndex[1].getCellsInRange('A19:B23')
+    },
+    "User information": await doc.sheetsByIndex[2].getCellsInRange('A10:E14'),
+    "Timesheet information": await doc.sheetsByIndex[3].getCellsInRange('B10:H21'),
+    "Project information": await doc.sheetsByIndex[4].getCellsInRange('B2:D5'),
+    "Sales": {
+      "Financial data": await doc.sheetsByIndex[5].getCellsInRange('B10:G17'),
+      "Financial Metrics": await doc.sheetsByIndex[5].getCellsInRange('K9:M13')
+    }
+  }
+
+  callback(mockData);
 }
 
-/**
- * Prints the names and majors of students in a sample spreadsheet:
- * @see https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- * @param {google.auth.OAuth2} auth The authenticated Google OAuth client.
- */
-function readData(auth: any, callback: any) {
-  const sheets = google.sheets({version: 'v4', auth});
-  sheets.spreadsheets.values.batchGet({
-    spreadsheetId: '1C_GqEIdO_F8M-WuxsYoFLK7-I8XNL6hmtr8V33wmGLU',
-    ranges: [
-      'Engineering!A6:R', 
-      'Company information!A10:B14',
-      'Company information!D10:F14',
-      'Company information!A19:B23',
-      'User information!A10:E14',
-      'Timesheet information!B10:H21',
-      'Project information!B2:D5',
-      'Sales information!B10:G17',
-      'Sales information!K9:M13'
-    ],
-  }, (err, res) => {
-    if (err) return console.log('The API returned an error: ' + err);
-    const rows = res.data.valueRanges;
-    callback(rows);
-  });
-}
-
-export default (credentials: any, callback: any) =>{
-  const {client_secret, client_id, redirect_uris} = credentials.installed;
-  const oAuth2Client = new google.auth.OAuth2(
-      client_id, client_secret, redirect_uris[0]);
-
-  // Check if we have previously stored a token.
-  getNewToken(oAuth2Client, callback);
+export default {
+  read
 };
